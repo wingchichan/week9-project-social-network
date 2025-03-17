ad7364cd25780828f9188d97278ce0fe0c8e1dac
wingchichan <w.chi.chan@gmail.com>
2025-03-17 08:57:31 +0000

    updated timeline page

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 
 export default async function TimelinePage() {
   const db = connect();
-  const data = await db.query(`SELECT * FROM social_posts`);
+  const data = await db.query(`SELECT social_posts.*, users.username
+FROM social_posts
+JOIN users ON social_posts.user_id = users.id`);
   //   returns the useful data we need
   const posts = data.rows;
   console.log(posts);
@@ -14,6 +16,7 @@ export default async function TimelinePage() {
           className="flex flex-col pt-10 odd:bg-white even:bg-gray-50 p-4 rounded-lg mb-1"
           key={post.id}
         >
+          <p className="font-bold underline">{post.username}</p>
           <p className="mb-4">{post.content}</p>
           {/* conditional rendering only if img url has a value */}
           {post.img_url && (
